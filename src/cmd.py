6dc9f0704207429ea76d089907b6acfe9698bbc9
RodenLuo <roden.luo@gmail.com>
2025-05-15 18:03:30 +0300

    Auto: Update highlight terminology in bundle_info.xml and enhance highlight functionality in cmd.py. Changed 'silhouette' to 'outline' in descriptions and added functionality for setting highlight color and width.

--- src/cmd.py
+++ src/cmd.py
@@ -2,6 +2,7 @@ from chimerax.core.commands import (
     CmdDesc, ColorArg, IntArg, StringArg, NoArg
 )
 from chimerax.atomic import ResiduesArg
+from chimerax.core.colors import Color
 
 # keep track of what we're highlighting
 _highlighted = {}  # residue -> (color, width)
@@ -20,21 +21,32 @@ highlight_info_desc = CmdDesc(
     synopsis='Print information about currently highlighted objects'
 ) 
 
-def add_highlights(session, residues, color="yellow", width=4):
+def add_highlights(session, residues, color=Color("yellow"), width=4):
+    """Add highlight outline to residues"""
+    
     for r in residues:
         _highlighted[r] = (color, width)
+        # Set the highlight color and width for this residue
+        r.atoms.selected = True
+        session.main_view.highlight_color = color.rgba
+        session.main_view.highlight_thickness = width
+        session.main_view.redraw_needed = True
 
 desc_add = CmdDesc(
-    synopsis='Add silhouette highlight to objects',
+    synopsis='Add highlight outline to objects',
     required=[("residues", ResiduesArg)],
     keyword=[("color", ColorArg), ("width", IntArg)],
 )
 
 def remove_highlights(session, residues):
+    """Remove highlight outline from residues"""
     for r in residues:
         _highlighted.pop(r, None)
+        # Deselect the atoms to remove the highlight
+        r.atoms.selected = False
+        session.main_view.redraw_needed = True
 
 desc_rm = CmdDesc(
-    synopsis='Remove silhouette highlight from objects',
+    synopsis='Remove highlight outline from objects',
     required=[("residues", ResiduesArg)],
 )
